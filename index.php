0763cae546a131ae275d9ccedb5a620eb0f6025c
vcburnett <victor@konversion.ca>
2017-05-04 17:03:46 -0400

    Initial Upload
    • Index.php structure
    • file structure

---
+++ index.php
@@ -0,0 +1,172 @@
+<!doctype html>
+<html lang="en">
+<!--<html lang="fr">-->
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1" name="viewport">
+	<!--<script src="//cdn.optimizely.com/js/{% code %}.js"></script>-->
+	<!-- Page metas -->
+	<!-- Social metas -->
+
+	<title>TITLE</title>
+	<link rel="icon" href="assets/images/favicon.png" type="image/x-icon"/>
+
+	<!-- stylesheets -->
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<!-- stylesheets -->
+
+	<!-- scripts -->
+</head>
+
+<body>
+
+	<header style="background-image:url('assets/images/hero-image.jpg');">
+		<div id="hero-logo">
+			<img src="assets/images/vcb-logo.svg" alt="Victor Burnett Design">
+		</div>
+		<div class="hero-content">
+			<h1>Tenderloin</h1>
+			<p>Beef meatball boudin ball tip capicola cupim biltong andouille pork. Prosciutto leberkas kielbasa, pastrami sirloin shank andouille flank ground round jerky filet mignon hamburger bresaola.</p>
+			<a href="javascript:void(0);" target="_self">Venison pork chop</a>
+			<a href="javascript:void(0);" target="_self">Chuck pig sausage</a>
+		</div>
+		<div class="hero-bottom-cover">
+			<img src="assets/images/waves.svg">
+		</div>
+	</header>
+
+	<section id="things-i-do">
+		<div class="section-intro">
+			<p>Pork chop tongue rump pastrami, ham hock pig beef landjaeger shoulder fatback brisket sirloin capicola ribeye. Ground round pancetta pork loin prosciutto bresaola short loin picanha t-bone short ribs turkey sausage pork chop chicken kevin capicola. Turducken tenderloin ham shankle. Doner bacon rump ham brisket turkey venison burgdoggen shoulder corned beef porchetta.</p>
+		</div>
+		<div class="main-services">
+			<div class="a-service">
+				<div class="a-service-thumb">
+					<img src="assets/images/thumb.svg" alt="Thumb">
+				</div>
+				<h2>Landjaeger</h2>
+				<p>Beef meatball boudin ball tip capicola cupim biltong andouille pork. Prosciutto leberkas kielbasa, pastrami sirloin shank andouille flank ground round jerky filet mignon hamburger bresaola.</p>
+			</div>
+			<div class="a-service">
+				<div class="a-service-thumb">
+					<img src="assets/images/thumb.svg" alt="Thumb">
+				</div>
+				<h2>Short ribs</h2>
+				<p>Beef meatball boudin ball tip capicola cupim biltong andouille pork. Prosciutto leberkas kielbasa, pastrami sirloin shank andouille flank ground round jerky filet mignon hamburger bresaola.</p>
+			</div>
+			<div class="a-service">
+				<div class="a-service-thumb">
+					<img src="assets/images/thumb.svg" alt="Thumb">
+				</div>
+				<h2>Hamburger</h2>
+				<p>Beef meatball boudin ball tip capicola cupim biltong andouille pork. Prosciutto leberkas kielbasa, pastrami sirloin shank andouille flank ground round jerky filet mignon hamburger bresaola.</p>
+			</div>
+			<div class="a-service">
+				<div class="a-service-thumb">
+					<img src="assets/images/thumb.svg" alt="Thumb">
+				</div>
+				<h2>Shank picanha</h2>
+				<p>Beef meatball boudin ball tip capicola cupim biltong andouille pork. Prosciutto leberkas kielbasa, pastrami sirloin shank andouille flank ground round jerky filet mignon hamburger bresaola.</p>
+			</div>
+		</div>
+		<div class="secondary-services">
+			<div class="sec-service">
+				<div class="sec-service-thumb">
+					<img src="assets/images/thumb.svg" alt="Thumb">
+				</div>
+				<h2>Prosciutto</h2>
+			</div>
+			<div class="sec-service">
+				<div class="sec-service-thumb">
+					<img src="assets/images/thumb.svg" alt="Thumb">
+				</div>
+				<h2>Beef ribs brisket</h2>
+			</div>
+			<div class="sec-service">
+				<div class="sec-service-thumb">
+					<img src="assets/images/thumb.svg" alt="Thumb">
+				</div>
+				<h2>Meatloaf</h2>
+			</div>
+			<div class="sec-service">
+				<div class="sec-service-thumb">
+					<img src="assets/images/thumb.svg" alt="Thumb">
+				</div>
+				<h2>Salami swine</h2>
+			</div>
+		</div>
+	</section>
+
+	<section id="portfolio">
+		<div id="portfolio-slider">
+			<ul>
+				<li>
+					<div class="slider-thumb">
+
+					</div>
+					<div class="slider-content">
+
+					</div>
+				</li>
+			</ul>
+		</div>
+	</section>
+
+	<section id="about-me">
+		<div class="about-intro">
+			<div class="about-logo">
+				<img src="assets/images/vcb-logo.svg" alt="Victor Burnett Design">
+			</div>
+			<div class="about-thumb-wrap">
+				<div class="about-thumb-content">
+					<h2>Short Ribs Bacon</h2>
+					<p>Tenderloin meatball shoulder</p>
+				</div>
+			</div>
+		</div>
+		<div class="about-content">
+			<div class="about-personal">
+				<h3>Corned beef shoulder</h3>
+				<p>Brisket bresaola burgdoggen jowl beef alcatra pork chop spare ribs t-bone meatball corned beef. Ham ground round doner sirloin jerky chuck. Drumstick pork cupim, jowl kielbasa pancetta short ribs alcatra. Ground round tail chicken hamburger tri-tip pork ham shank turducken fatback.</p>
+				<p>Bacon ipsum dolor amet shoulder ribeye tongue, short ribs landjaeger pancetta pork andouille doner frankfurter drumstick meatloaf. Ham hamburger picanha, kevin tenderloin pork belly bacon pig beef cow.</p>
+				<a href="javasctip:void(0);" target="_self">View my blog</a>
+			</div>
+			<div class="about-work">
+				<h3>Drumstick</h3>
+				<p>Bacon ipsum dolor amet shoulder ribeye tongue, short ribs landjaeger pancetta pork andouille doner frankfurter drumstick meatloaf. Ham hamburger picanha, kevin tenderloin pork belly bacon pig beef cow.</p>
+				<p>Brisket bresaola burgdoggen jowl beef alcatra pork chop spare ribs t-bone meatball corned beef. Ham ground round doner sirloin jerky chuck. Drumstick pork cupim, jowl kielbasa pancetta short ribs alcatra. Ground round tail chicken hamburger tri-tip pork ham shank turducken fatback.</p>
+				<a href="javasctip:void(0);" target="_self">Download CV</a>
+			</div>
+		</div>
+	</section>
+
+	<section id="contact">
+		<div class="section-intro">
+			<p>Cupim porchetta capicola pastrami pork chop kevin pancetta tail shankle swine tri-tip ribeye. Tail jerky landjaeger meatloaf, sausage shank hamburger beef kielbasa jowl swine strip steak ham hock. Landjaeger prosciutto leberkas pork loin.</p>
+		</div>
+		<div class="form">
+			<form>
+				<label>Name</label>
+				<input type="text">
+				<label>Email</label>
+				<input type="text">
+				<label>Message</label>
+				<textarea></textarea>
+			</form>
+		</div>
+		<div class="form-footnote">
+			<p>Cow bresaola tenderloin filet mignon jowl swine corned beef sirloin pork chop burgdoggen cupim ground round. Hamburger meatball tongue flank sirloin swine meatloaf short loin boudin bresaola.</p>
+		</div>
+	</section>
+
+	<footer>
+		<div class="ft-text">
+			<p>&copy; 2005-2017 Victor Burnett. All rights reserved.</p>
+		</div>
+		<div class="ft-logo">
+			<img src="assets/images/vcb-logo.svg" alt="Victor Burnett Design">
+		</div>
+	</footer>
+
+</body>
+</html>
