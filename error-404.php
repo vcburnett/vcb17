55b54d76b5176048b2e62c1003e7c34c2e0ef052
vcburnett <victor@konversion.ca>
2017-06-06 15:25:52 -0400

    case study and error page

--- error-404.php
+++ error-404.php
@@ -49,65 +49,14 @@
 		</div>
 	</nav>
 
-	<header id="header-case-study">
+	<header id="header-error">
+		<div class="error-wrap">
+			<div class="error-content">
+				<h1>What the hell were you looking for?</h1>
+				<p>And here I was thinking I had no missing content here... but boy was I wrong! Go back to the <a href="javascript:void(0);">home page</a> and maybe start over.</p>
+			</div>
+		</div>
 	</header>
-
-	<div class="content-wrapper">
-
-		<div class="content-top">
-			<div class="cs-info-1">
-				<a onclick="window.history.back()" class="btn-back-mob">Back</a>
-				<p class="year">2017</p>
-				<p><span class="txt-client-name">Client Name</span> Bacon frankfurter pork pork chop. Andouille boudin frankfurter, tongue shoulder pork loin drumstick bacon landjaeger. Ground round rump chicken brisket meatloaf cow bacon alcatra sirloin venison. Short loin pancetta rump filet mignon capicola burgdoggen strip steak ribeye chuck.</p>
-				<a onclick="window.history.back()" class="btn-back-desk">Back</a>
-			</div>
-			<div class="cs-info-2">
-				<div class="cs-info-left">
-					<div class="cs-info-int-left">
-						<p>Services</p>
-					</div>
-					<div class="cs-info-int-right">
-						<p>Branding</p>
-						<p>UI Design</p>
-						<p>UX Design</p>
-					</div>
-				</div>
-				<div class="cs-info-right">
-					<div class="cs-info-int-left">
-						<p>Tools</p>
-					</div>
-					<div class="cs-info-int-right">
-						<p>Photoshop</p>
-						<p>Illustrator</p>
-						<p>Axure</p>
-						<p>Sublime Text</p>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="post-content">
-			<div class="post-image">
-				<img src="assets/images/temp/image-example.jpg" alt="alt of image">
-			</div>
-			<div class="post-text">
-				<p>Filet mignon kielbasa kevin, t-bone beef ribs ball tip salami tongue. Strip steak swine pastrami tongue. Turkey cupim ribeye t-bone turducken pancetta biltong ham hock pork belly leberkas capicola pig beef ribs tenderloin shoulder.</p>
-			</div>
-			<div class="post-image">
-				<img src="assets/images/temp/image-example.jpg" alt="alt of image">
-			</div>
-			<div class="post-image">
-				<img src="assets/images/temp/image-example.jpg" alt="alt of image">
-			</div>
-		</div>
-
-		<div class="end-icon">
-			<div class="end-icon-wrap">
-				<img src="assets/images/temp/end-icon.png" alt="Name of Client">
-			</div>
-		</div>
-
-	</div>
 
 	<section id="contact">
 		<div class="section-intro">
