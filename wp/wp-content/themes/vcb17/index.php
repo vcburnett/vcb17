52b4c48b1ab9a71af3b4ee20bd2af2fb63b81129
vcburnett <victor@konversion.ca>
2017-06-07 17:11:47 -0400

    Updates on WP

--- wp/wp-content/themes/vcb17/index.php
+++ wp/wp-content/themes/vcb17/index.php
@@ -6,6 +6,9 @@
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
  * E.g., it puts together the home page when no home.php file exists.
+
+portfolio_header
+
  *
  */
 
@@ -18,6 +21,7 @@ get_header(); ?>
 			<header id="header-internal-page" style="background-image:url('<?php echo get_template_directory_uri(); ?>/assets/images/temp/internal-hero-image.jpg');"></header>
 
 			<div class="content-wrapper">
+
 				<?php
 				if ( have_posts() ) :
 
